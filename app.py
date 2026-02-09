a39d7687989644cf13d55f6059992f456530a5c5
LahireNeto <lahire_neto@hotmail.com>
2026-02-08 21:27:00 -0300

    Mudança na fonte da base de dados

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ st.set_page_config(
 )
 
 # --- Carregamento dos dados ---
-df = pd.read_csv("https://raw.githubusercontent.com/vqrca/dashboard_salarios_dados/refs/heads/main/dados-imersao-final.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/LahireNeto/Imersao-Dados-Python-Alura/refs/heads/main/dados_limpos_imersao.csv")
+
 
 # --- Barra Lateral (Filtros) ---
 st.sidebar.header("🔍 Filtros")
